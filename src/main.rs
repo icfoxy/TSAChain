4813580ff999ed4a393fbe003f211e32a8735cab
unknown <mxh15715247587@gmail.com>
2024-06-30 12:52:13 +0800

    c1

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod tsa;
+mod chain_server;
+mod wallet_server;
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/tsa/mod.rs
@@ -0,0 +1,17 @@
+const UNIT_NUM:i128=2000;
+const TIMES:i128=1000;
+pub struct Puzzle{
+
+}
+pub struct Solution{
+
+}
+//TODO:完成tsa函数
+pub fn do_tsa(puzzle:Puzzle)->Option<Solution>{
+    return None;
+}
+
+//TODO:完成验证函数
+pub fn valid_solution(puzzle:Puzzle,solution:Solution)->bool{
+    return true;
+}
