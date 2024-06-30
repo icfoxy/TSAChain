48bdbacb3e082859c10cee2e46caa5b90ade84fa
unknown <mxh15715247587@gmail.com>
2024-06-30 16:00:56 +0800

    c2

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,6 @@ mod chain_server;
 mod wallet_server;
 fn main() {
     println!("Hello, world!");
+    let su=tsa::Solution::new(&vec![10;10], &vec![100;10]);
+    su.print();
 }
--- src/tsa/mod.rs
+++ src/tsa/mod.rs
@@ -1,11 +1,55 @@
-const UNIT_NUM:i128=2000;
-const TIMES:i128=1000;
-pub struct Puzzle{
+use core::task;
 
+const UNIT_NUM:i64=2000;
+const TIMES:i64=1000;
+pub struct Puzzle{
+    tasks:Vec<i32>,
+    vms:Vec<i32>,
+    expect:f32,
 }
 pub struct Solution{
+    assign:Vec<Vec<i32>>,
+    velocity:Vec<i32>,
+    max_response_time:f32,
+}
+
+impl Solution{
+    pub fn new(vms:&Vec<i32>,tasks:&Vec<i32>)->Self{
+        let mut a=vec![vec![0;tasks.len()+1];vms.len()];
+        let v=vec![0;tasks.len()];
+        for i in 0..vms.len(){
+            a[i][0]=vms[i];
+        }
+        //TODO:应当改成随机分配
+        for i in 1..tasks.len()+1{
+            a[0][i]=tasks[i-1];
+        }
+        return Self{
+            assign:a,
+            velocity:v,
+            max_response_time:std::f32::MAX,
+        }
+    }
+    pub fn update_velocity(&mut self,best_solution:&Solution){
+        //TODO:没写呢
+    }
+    pub fn update_assign(&mut self){
+        //TODO:没写呢
+    }
+    pub fn update_max_response_time(&mut self){
+        //TODO:没写呢
+    }
+    pub fn print(&self){
+        for i in 0..self.assign.len(){
+            for j in 0..self.assign[i].len(){
+                print!{"{} ",self.assign[i][j]};
+            }
+            println!("");
+        }
+    }
 
 }
+
 //TODO:完成tsa函数
 pub fn do_tsa(puzzle:Puzzle)->Option<Solution>{
     return None;
